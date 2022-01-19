3d0f51651d7ca64f73179b7e133261f2e93baa9e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-19 16:29:17 -0600

    update

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,10 +1,12 @@
 import './feed.css';
 import Share from '../share/Share';
+import Post from '../post/Post';
 const Feed = () => {
   return (
     <div className ='feed'>
     <div className ='feedWrapper'>
       <Share/>
+      <Post/>
     </div>
     </div>
   );
