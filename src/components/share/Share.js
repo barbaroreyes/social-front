2b69785054c52d71438c3e9801e033a6be1c1c12
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-18 19:40:02 -0600

    update

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,11 +1,20 @@
 import  './share.css';
-
+import {PermMedia} from '@material-ui/icons'
 const Share = () => {
   return (
     <div className="share">
       <div className="shareWrapper">
           <div className="shareTop">
-              sharetop
+              <img  className = "sharePicture"src='/assets/usa.jpg' alt ='person'/>
+              <input placeholder='wat in your mind' className="shareInput"/>
+          </div>
+          <hr className="shareHr"/>
+          <div className="shareBotton">
+             <div className="shareOption">
+                 <PermMedia className="shareIcon"/>
+                 <span className="shareOptionText">Photo or video</span>
+
+             </div>
           </div>
       </div>
     </div>
