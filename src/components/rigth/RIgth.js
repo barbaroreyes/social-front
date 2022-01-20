188213735e397f7a2674368101b5b3ee5aa282fb
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-19 19:06:43 -0600

    update

--- src/components/rigth/RIgth.js
+++ src/components/rigth/RIgth.js
@@ -3,7 +3,14 @@ import  './rigth.css';
 const Rigth = () => {
   return (
     <div className ='right'>
-      Rigth 
+      <div className="rigthWrapper">
+        <div className="birthContainer">
+          <img className="birthImg"  src='/assets/birthDay.jpg'   alt="profile"/>
+          <span className="birthText">
+            <b>pepe gomez</b><hr/>and <b>3 other </b>have  a birthDay Today
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
