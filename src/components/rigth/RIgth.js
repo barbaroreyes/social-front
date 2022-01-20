9b722c7f2911c0c3d7894301d484d23f5233e51c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-20 17:41:21 -0600

    update

--- src/components/rigth/RIgth.js
+++ src/components/rigth/RIgth.js
@@ -10,6 +10,17 @@ const Rigth = () => {
             <b>pepe gomez</b><hr/>and <b>3 other </b>have  a birthDay Today
           </span>
         </div>
+        <img className="rigtAdd" src='/assets/ad.png' alt="profile"/>
+        <h4 className="rigth">Online Frieds</h4>
+        <ul className="rigthFriendsList">
+          <li className="friend">
+            <div className="friendProfileContainer">
+              <img className="friendProfileImage" src ='/assets/ratoncito.jpg' alt ='profile' />
+              <span className="friendOnline"></span>
+            </div>
+            <span className="friendUsername">Papo Reyes</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
