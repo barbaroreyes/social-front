c7087f45263aaf826206261707cba62dd70e4d12
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-21 18:56:37 -0600

    update-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Home from './pages/Home';
 import {Routes, Route} from 'react-router-dom'
 import './App.css';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Profile from './pages/profile/Profile';
 
 
 function App() {
@@ -8,6 +11,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/> 
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/profile/:username' element={<Profile/>}/>
+
       </Routes>
      
     </div>
