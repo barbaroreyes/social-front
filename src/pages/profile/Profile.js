e0a24e9386c73e67e2f388c5677de995b738d551
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-21 20:58:47 -0600

    update

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -4,7 +4,7 @@ import Feed from '../../components/feed/Feed';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Rigth from '../../components/rigth/RIgth';
 const Profile = () => {
-  
+  const PF = process.env.REACT_APP_PUBLIC_FORDER ;
   return (
     <>
     <TopBar/>
@@ -13,8 +13,8 @@ const Profile = () => {
      <div className = 'profileRight'>
          <div className = 'profileRigthTop'>
              <div className = 'profileCover'>
-             <img className = 'profileCoverImg' src='/assets/post/028.jpg' alt=''/>
-             <img className = 'profileUserimg' src='/assets/usa.jpg' alt=''/>
+             <img className = 'profileCoverImg' src={`${PF}post/028.jpg`} alt=''/>
+             <img className = 'profileUserimg' src={`${PF}usa.jpg`} alt=''/>
              </div>
             <div className = 'profileInfo'>
                 <h4 className = 'profileInfoName'>Name</h4>
